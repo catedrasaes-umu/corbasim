7849ac95afa8572d8ae949a875136b4769f8c311
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2011-11-23 16:51:03 +0000

    Brainstorming

--- corbasim/app/view/Servant.cpp
+++ corbasim/app/view/Servant.cpp
@@ -31,6 +31,8 @@ Servant::Servant(QMdiArea * area,
 
     m_menu = new QMenu(menu_entry);
     // TODO
+    m_menu->addAction("Self-stimulator");
+    m_menu->addAction("Asynchronous triggers");
     m_menu->addSeparator();
     m_menu->addAction("&Delete", this, SLOT(deleteServant()));
 }
