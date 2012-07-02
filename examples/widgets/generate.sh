477e8b1188ef6f27eaada385154708ac2f2e741e
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-07-02 21:03:38 +0000

    Deprecated examples

--- examples/widgets/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASEDIR=`dirname $0`
-
-for i in ${BASEDIR}/*.idl; do
-    echo "Generando $i................................." 
-    ${BASEDIR}/../../corbasim_idl/bin/corbasim_idl -Gclient -Gserver -Ginjector $@ $i
-done
-
