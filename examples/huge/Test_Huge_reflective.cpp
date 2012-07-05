d46ca5ef47f53fec007130d998d34ce7c169949f
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-07-05 17:33:49 +0000

    Generated example

---
+++ examples/huge/Test_Huge_reflective.cpp
@@ -0,0 +1,11 @@
+
+#include "hugeC.h"
+#include "huge_adapted.hpp"
+#include "huge_servant.hpp"
+#include <corbasim/core/reflective.hpp>
+
+extern "C" const corbasim::core::interface_reflective_base * corbasim_reflective_Test_Huge()
+{
+	return corbasim::core::interface_reflective< Test::Huge >::get_instance();
+}
+
