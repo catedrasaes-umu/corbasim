c21996ab643a7f4f6ddb5ecb2a4ef651d7eff74d
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-07-17 21:26:06 +0000

    Fixing some installation issues.

--- corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
+++ corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
@@ -1,17 +1,23 @@
 
-set(CORBASIM_INSTALL_PREFIX /tmp/corbasim_install)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+set(CMAKE_INSTALL_RPATH ".")
+
+set(CORBASIM_INSTALL_PREFIX /tmp/corbasim)
 
 include_directories(${CORBASIM_INSTALL_PREFIX}/include)
 link_directories(${CORBASIM_INSTALL_PREFIX}/lib)
 
 # Boost C++ Libraries
-include_directories(/home/asenac/local/boost/1_49_0/include)
-link_directories(/home/asenac/local/boost/1_49_0/lib)
+include_directories(/usr/include)
+link_directories(/usr/lib)
 
 # ACE+TAO
 include_directories(
-    /usr/include
-    /usr/include
-    /usr/include/orbsvcs)
-link_directories(/usr/lib)
+    
+    
+    /orbsvcs)
+link_directories()
+
+set(CORBASIM_ORB_LIBS
+    TAO;TAO_PortableServer;TAO_AnyTypeCode;TAO_CosNaming;TAO_ObjRefTemplate)
 
