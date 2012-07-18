0260f3e1306331c5120dbb5e651c4f85f9fc0e07
andres@senac.es <andres@senac.es@6a887da5-8e5e-6d2e-f52f-3bb2b046c35a>
2012-07-18 22:00:29 +0000

    omniORB4. just one step more

--- corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
+++ corbasim_idl/share/corbasim/cmake/CorbasimEnv.cmake
@@ -8,21 +8,27 @@ link_directories(${CORBASIM_INSTALL_PREFIX}/lib)
 include_directories(/usr/include)
 link_directories(/usr/lib)
 
-# ACE+TAO
-include_directories(
-    
-    
-    /orbsvcs)
-link_directories()
+# ORB
+set(CORBASIM_ORBIMPL
+    TAO)
 
 set(CORBASIM_ORB_LIBS
     TAO;TAO_PortableServer;TAO_AnyTypeCode;TAO_CosNaming;TAO_ObjRefTemplate)
+
+include_directories(/orbsvcs)
+link_directories()
+
+set(CORBASIM_ORB_IDL_COMPILER
+    tao_idl)
+
+set(CORBASIM_ORB_IDL_COMPILER_OPTIONS
+    )
 
 # Libraries for clients
 set(CORBASIM_CLIENT_LIBS 
     corbasim corbasim_qt corbasim_reflective_gui)
 
-# Libraries for clients
+# Libraries for servers
 set(CORBASIM_SERVER_LIBS 
     corbasim)
 
