f3bbd1a95e0b07ffad47c95448f9bfd73fc54a54
asenac <andres@senac.es>
2013-12-09 22:46:17 +0000

    Removed trailing whitespaces.

--- org.csu.corbasim/src/templates/FileNameHelper.java
+++ org.csu.corbasim/src/templates/FileNameHelper.java
@@ -5,38 +5,38 @@ import org.csu.idl.idlmm.TranslationUnit;
 
 public class FileNameHelper {
 
-	public static String getFileName(TranslationUnit tu) {
-		return removeExtension(tu.eResource().getURI().toString()); // why?
-	}
-	
-	public static String getFileName(Include include) {
-		return removeExtension(include.getImportURI());
-	}
+    public static String getFileName(TranslationUnit tu) {
+        return removeExtension(tu.eResource().getURI().toString()); // why?
+    }
 
-	/**
-	 * Private internal method to extract file's name (without extension).
-	 * @param s path to a file.
-	 * @return file's name without extension.
-	 */
-	private static String removeExtension(String s) {
+    public static String getFileName(Include include) {
+        return removeExtension(include.getImportURI());
+    }
 
-		String separator = System.getProperty("file.separator");
-		String filename;
+    /**
+     * Private internal method to extract file's name (without extension).
+     * @param s path to a file.
+     * @return file's name without extension.
+     */
+    private static String removeExtension(String s) {
 
-		// Remove the path upto the filename.
-		int lastSeparatorIndex = s.lastIndexOf(separator);
-		if (lastSeparatorIndex == -1) {
-			filename = s;
-		} else {
-			filename = s.substring(lastSeparatorIndex + 1);
-		}
+        String separator = System.getProperty("file.separator");
+        String filename;
 
-		// Remove the extension.
-		int extensionIndex = filename.lastIndexOf(".");
-		if (extensionIndex == -1)
-			return filename;
+        // Remove the path upto the filename.
+        int lastSeparatorIndex = s.lastIndexOf(separator);
+        if (lastSeparatorIndex == -1) {
+            filename = s;
+        } else {
+            filename = s.substring(lastSeparatorIndex + 1);
+        }
 
-		return filename.substring(0, extensionIndex);
-	}
+        // Remove the extension.
+        int extensionIndex = filename.lastIndexOf(".");
+        if (extensionIndex == -1)
+            return filename;
+
+        return filename.substring(0, extensionIndex);
+    }
 
 }
