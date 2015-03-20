537d2e266772414c8ec49cd6478fbdbef618102e
Andres Senac <andres@senac.es>
2015-03-20 08:40:14 +0000

    Fix linking issues.

--- corbasim/cosnaming/CosNaming_NamingContextExt_reflective.cpp
+++ corbasim/cosnaming/CosNaming_NamingContextExt_reflective.cpp
@@ -4,6 +4,14 @@
 #include "Cosnaming_servant.hpp"
 #include <corbasim/core/reflective.hpp>
 
+namespace corbasim
+{
+    namespace core
+    {
+        template struct interface_reflective< CosNaming::NameContextExt >;
+    }
+}
+
 extern "C" const corbasim::core::interface_reflective_base * corbasim_reflective_CosNaming_NamingContextExt()
 {
     return corbasim::core::interface_reflective< CosNaming::NamingContextExt >::get_instance();
